b489cd67216b228fed5c64dead4619bc54cc2271
Zephyros1938.org <xtrau505true@gmail.com>
2024-10-03 20:47:39 +0000

    Modified the main.rs file to have a descrition of what to put inside of there.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 pub mod zephyros1938_util;
 pub mod zephyros1938_util_old;
 pub mod main_modules;
+/*
+    Hi welcome to the mainscript
+    Generally you'd put something from main_modules::testing in here to test out the runners.
+    Its reccommended you only put functions in here, and NOT code.
+    Reference functions from the other crates/mods. 
+*/
+
 
 fn main() {
     main_modules::testing::_runner_2();
